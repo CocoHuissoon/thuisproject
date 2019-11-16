b8dcca086157a082021358e154600c26fb95e8c9
chuis <chuissoon@hotmail.com>
2019-11-16 12:41:54 +0100

    added thymeleaf

--- src/main/java/nl/chu/docker/dockerspring/resource/HelloResource.java
+++ src/main/java/nl/chu/docker/dockerspring/resource/HelloResource.java
@@ -1,5 +1,6 @@
 package nl.chu.docker.dockerspring.resource;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloResource {
 
     @RequestMapping
-    public String docker(){
-        return "dockerdockerdocker";
+    public String docker(Model model){
+        return "index.html";
     }
 
 
